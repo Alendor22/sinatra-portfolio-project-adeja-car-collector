c8df78b12e077e756644df8a8a90bfbe9ca00704
Adeja <alendor6@gmail.com>
2020-02-10 21:19:47 -0500

    added name to users & validator for name, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_many :user_cars
   has_many :cars, through: :user_cars
-  validates :username, presence: true
+  has_secure_password
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200211010742) do
+ActiveRecord::Schema.define(version: 20200211020925) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "year"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20200211010742) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
