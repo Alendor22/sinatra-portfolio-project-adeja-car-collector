f5c365e3ddc57c6d2ea0624eca12f973bcabbb0b
Adeja <alendor6@gmail.com>
2020-02-16 11:21:24 -0500

    updated routes in cars_controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,53 +1,54 @@
 class CarsController < ApplicationController
 
   #define car index action
-  get '/users/cars' do
+  get '/cars' do
     #get all cars
     #create an instance variable to hold them
-    @car = Car.all
+    @cars = Car.all
     # render the cars index view
-    erb :"/users/cars/index"
+    erb :"/cars/index"
   end
 
   # cars create action
-  post '/users/cars' do 
-    @car = Car.new(params["car"])
-      if @car.save
+  post '/cars' do 
+    @cars = Car.new(params["car"])
+      if @cars.save
         session[:car_id] = car.id 
-        redirect to "/users/cars"
+        redirect to "/cars"
       else
         @errors = car.errors.full_message
-        erb :"/users/cars/new"
+        erb :"/cars/new"
       end
   end
 
   #cars show action via dynamic route variable
   get '/cars/:id' do
-    @car = Car.find_by(id: params[:id])
-    erb :"/users/cars/show"
+    @cars = Car.find_by(id: params[:id])
+    #binding.pry
+    erb :"/cars/show"
   end
 
   # cars get edit action
-  get '/users/cars/:id/edit' do 
-    @car = Car.find_by_id(params[:id])
-    erb :"/users/cars/edit"
+  get '/cars/:id/edit' do 
+    @cars = Car.find_by_id(params[:id])
+    erb :"/cars/edit"
   end
 
   #cars update action
-  patch '/users/cars/:id' do 
-    @car = User.find_by_id(params[:id])
-    if @user.authenticate(params[:user][:password]) && @user.update(params   [:user])
-      redirect to "/users/#{@car.id}"
+  patch '/cars/:id' do 
+    @cars = User.find_by_id(params[:id])
+    if @user.authenticate(params[:user][:password]) && @user.update(params[:user])
+      redirect to "/cars/#{@car.id}"
     else
-      erb :"/user/cars/show"
+      erb :"/cars/show"
     end
   end
 
   # cars destroy action
-  delete '/users/cars/:id' do
-    @car = Car.find_by_id(params[:id])
-    @car.destroy
-    redirect to "/users/cars/index"
+  delete '/cars/:id' do
+    @cars = Car.find_by_id(params[:id])
+    @cars.destroy
+    redirect to "/cars/index"
   end
 
 end
