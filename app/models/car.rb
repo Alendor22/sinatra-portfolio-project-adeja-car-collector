a1c07a085c951d838637426b2fed24a80b41879f
Adeja <alendor6@gmail.com>
2020-02-21 12:04:30 -0500

    added has_many and belongs_to relationships

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,8 @@
 class Car < ActiveRecord::Base
 
-  has_many :user_cars
-  has_many :users, through: :user_cars
+  belongs_to :owner, class_name: "User", foreign_key: :owner_id
+  has_many :favorites
+  has_many :fans, through: :favorites, source: :user
+  has_many :followers, class_name: "User", foreign_key: :favorite_car_id
 
 end
