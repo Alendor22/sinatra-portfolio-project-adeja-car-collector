51df345db9664d4f018b12f8615209659d525a8b
Adeja <alendor6@gmail.com>
2020-02-21 12:06:49 -0500

    updates to CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,22 @@ class UsersController < ApplicationController
     erb :"/users/index"
   end
 
-  # users new action
+  # users new action - working
   get '/signup' do
     erb :"users/new"
   end
 
-  # users create action
+  # users create action - working
   post '/users' do 
+    
     @user = User.new(params["user"])
-      
       if @user.save
         session[:user_id] = @user.id 
         redirect to "/users"
       else
         erb :"/users/new"
       end
-    end
+  end
 
     #users show action via dynamic route variable
     get '/users/:id' do
@@ -34,9 +34,11 @@ class UsersController < ApplicationController
 
     # users get edit action
     get '/users/:id/edit' do 
-      @user = User.find_by_id(params[:id])
-      if @user == current_user
+      @user = User.find_by(id: params[:id])
+      
+      if current_user.id == @user.id 
         # show the form
+        #binding.pry
       erb :"/users/edit"
       else
         redirect to "/"
@@ -46,23 +48,24 @@ class UsersController < ApplicationController
     #users update action
     patch '/users/:id' do
       @user = User.find_by_id(params[:id])
-      if @user.authenticate(params[:user][:password]) && @user.update(params[:user])
+      if logged_in?
+        #binding.pry
+        @user.update#(:location current_user.location, :favorite_car current_user.favorites)
+        #map { |car| car.year }, current_user.favorite_cars.map { |car| car.make }, current_user.favorite_cars.map { |car| car.model })
         redirect to "/users/#{@user.id}"
-      else
-        erb :"/user/show"
       end
     end
 
     # users destroy action
     delete '/users/:id' do
-       @user = User.find_by_id(params[:id])
-        if @user.id != current_user.id
-          redirect to "/users/index"
-        else
-         @user.destroy
-         session.clear
-         redirect "/users"
-        end
+      @user = User.find_by_id(params[:id])
+      if current_user.id != @user.id
+        redirect to "/users/index"
+      else
+        @user.destroy
+        session.clear
+        redirect "/users"
+      end
     end
 
 end
